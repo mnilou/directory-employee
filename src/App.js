69a94fb6f6de2bf54e02668be3839d70bf96ded9
Nilou Mostofi <nilou@km-direct.com>
2020-11-05 19:44:08 -0800

    file transfer from repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="">
-     App
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Use the search box to find an employee by name.</p>
+      </div>
+    )
+  }
+
+export default Header;
