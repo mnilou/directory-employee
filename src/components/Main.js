587350c56b1046fdeef9dc324a6a83e5ad8c4724
Nilou Mostofi <nilou@km-direct.com>
2020-11-07 08:58:13 -0800

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from "react";
 import DataArea from "./DataArea";
-// import "../styles/Main.css";
 
 function Main () {
     return (
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import "../styles/Wrapper.css";
 
 function Wrapper(props) {
   return <div className="Wrapper">{props.children}</div>;
